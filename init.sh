5c138c1701afc89dd1cdf4738e61e7eed2b9ae2b
bagostini <bagostini@daitangroup.com>
2016-04-14 14:14:08 -0300

    . Now using TPM (Tmux Package Manager) . Added a plugin: Tmux Yank (it copies things with 'y' to your system clipboard) . Added some lines to avoid bugs with UTF8 . Increased the log bugger to 50000 . Added new key binds to go next/previous window

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ln -sf ~/tmux/.tmux.conf ~/.tmux.conf
-tmux start-server \; source ~/.tmux.conf
 sudo apt-get install -y xclip
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux start-server \; source ~/.tmux.conf
 echo "Done"
