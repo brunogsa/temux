1081a0821e3dd83f2ede1b8d0f7cbab2ab92e470
Bruno Agostini <brunogsa.92@gmail.com>
2016-05-03 04:26:07 -0300

    Improved init.sh to automatically upgrade tmux to v2.0

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 ln -sf ~/tmux/.tmux.conf ~/.tmux.conf
 sudo apt-get install -y xclip
+
+# Update tmux to 1.9, if it's the case
+sudo apt-get update
+sudo apt-get install -y python-software-properties software-properties-common
+sudo add-apt-repository -y ppa:pi-rho/dev
+sudo apt-get update
+sudo apt-get install -y tmux=2.0-1~ppa1~t
+
 tmux start-server \; source ~/.tmux.conf
 echo "Done"
