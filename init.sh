aebba1ac3c3ecce515d817ff455beafd35c28c23
Bruno Agostini <bagostini@daitangroup.com>
2016-05-11 18:09:01 -0300

    . Changed style from style

--- init.sh
+++ init.sh
@@ -8,6 +8,7 @@ sudo apt-get install -y python-software-properties software-properties-common
 sudo add-apt-repository -y ppa:pi-rho/dev
 sudo apt-get update
 sudo apt-get install -y tmux=2.0-1~ppa1~t
+sudo apt-get install -y fonts-powerline
 
 tmux start-server \; source ~/.tmux.conf
 echo "Done"
