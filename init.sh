71fa8b16005e342725c0071eb02d73aa3b107d06
Bruno Agostini <brunogsa.92@gmail.com>
2016-12-06 03:04:42 -0200

    , We're now installing tmux from source . Added style to active window . Added gitignore

--- init.sh
+++ init.sh
@@ -7,7 +7,16 @@ sudo apt-get update
 sudo apt-get install -y python-software-properties software-properties-common
 sudo add-apt-repository -y ppa:pi-rho/dev
 sudo apt-get update
-sudo apt-get install -y tmux=2.0-1~ppa1~t
+
+wget https://github.com/tmux/tmux/releases/download/2.3/tmux-2.3.tar.gz
+tar -xzf tmux-2.3.tar.gz
+cd tmux-2.3/
+sudo apt-get install -y libevent-dev ncurses-dev
+./configure
+sudo make
+sudo make install
+cd -
+
 sudo apt-get install -y fonts-powerline
 
 tmux start-server \; source ~/.tmux.conf
