4c2fca9a0e5486112da5fab99e8af9f2f7030655
Bruno Agostini <brunogsa.92@gmail.com>
2017-11-19 20:53:42 -0200

    Tmux is now installed directly via apt

--- init.sh
+++ init.sh
@@ -2,20 +2,13 @@
 ln -sf ~/tmux/.tmux.conf ~/.tmux.conf
 sudo apt-get install -y xclip
 
-# Update tmux to 1.9, if it's the case
 sudo apt-get update
 sudo apt-get install -y python-software-properties software-properties-common
 sudo add-apt-repository -y ppa:pi-rho/dev
 sudo apt-get update
 
-wget https://github.com/tmux/tmux/releases/download/2.3/tmux-2.3.tar.gz
-tar -xzf tmux-2.3.tar.gz
-cd tmux-2.3/
-sudo apt-get install -y libevent-dev ncurses-dev
-./configure
-sudo make
-sudo make install
-cd -
+# Install Tmux
+sudo apt-get install -y tmux
 
 sudo apt-get install -y fonts-powerline
 
