5353de508c729c0fa44df7166c9beaedbf33b526
Bruno Agostini <brunogsa.92@gmail.com>
2021-01-07 12:36:21 -0300

    Adjustments for Ubuntu 20

--- init.sh
+++ init.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-set -e
-
 ln -sf ~/tmux/.tmux.conf ~/.tmux.conf
 sudo apt-get install -y xclip
 
 sudo apt-get update
-sudo apt-get install -y python-software-properties software-properties-common
-sudo add-apt-repository -y ppa:pi-rho/dev
-sudo apt-get update
+sudo apt-get install -y software-properties-common
 
 # Install Tmux
 sudo apt-get install -y tmux
